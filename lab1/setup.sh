ad3cd1c2e835d759b932e8fd1f25dfa56edff039
CARLOS REBOLLO <crebollo@gmail.com>
2026-03-28 12:33:33 -0300

    Update setup.sh

--- lab1/setup.sh
+++ lab1/setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# Remove o IP da interface correta que apareceu no seu print
+
+# Espera 10 segundos para o terminal carregar na tela
+sleep 10
+
+# Agora sim, sabota a rede
 ip addr flush dev enp1s0
 ip link set enp1s0 down
 ip route del default
-echo > /etc/resolv.conf
+echo "" > /etc/resolv.conf
+
+# Limpa a tela e avisa o aluno
+clear
+echo "################################################"
+echo "# ALERTA: CONEXÃO DE REDE PERDIDA!             #"
+echo "# Sua missão: Restaurar a conectividade.       #"
+echo "################################################"
