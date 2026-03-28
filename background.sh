d88246275ee6be2af3ebc33326f40afba279fe1a
CARLOS REBOLLO <crebollo@gmail.com>
2026-03-28 11:32:44 -0300

    Update background.sh

--- background.sh
+++ background.sh
@@ -3,6 +3,6 @@
 ip route del default
 
 # Bloqueia tráfego de saída via Firewall (opcional, mais agressivo)
-#iptables -A OUTPUT -o eth0 -j DROP
+iptables -A OUTPUT -o eth0 -j DROP
 
 # Mantém apenas a interface de loopback e a rede interna se necessário
