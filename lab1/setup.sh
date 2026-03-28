dc2693393866387c249788c723c29a514f73646e
CARLOS REBOLLO <crebollo@gmail.com>
2026-03-28 12:30:15 -0300

    Update setup.sh

--- lab1/setup.sh
+++ lab1/setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-# 1. Remove o IP da interface principal (geralmente eth0 ou ens3)
-# O Killercoda usa eth0 por padrão
-ip addr flush dev eth0
-
-# 2. Desativa a interface para o aluno ter que dar 'up'
-ip link set eth0 down
-
-# 3. Remove o arquivo de DNS para simular perda total
-echo "" > /etc/resolv.conf
-
-# 4. Mensagem de boas vindas no terminal
-echo "O sistema de rede foi corrompido. Boa sorte, Administrador!" > /etc/motd
+# Remove o IP da interface correta que apareceu no seu print
+ip addr flush dev enp1s0
+ip link set enp1s0 down
+ip route del default
