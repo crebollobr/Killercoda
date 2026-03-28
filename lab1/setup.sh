835833c3baecdb4b316d6b16a88f6aa5d2d50551
CARLOS REBOLLO <crebollo@gmail.com>
2026-03-28 12:49:35 -0300

    Update setup.sh

--- lab1/setup.sh
+++ lab1/setup.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-# Espera o sistema estabilizar
-sleep 5
+# Espera o terminal abrir
+sleep 10
 
-# 1. Derruba a interface (Isso desativa a camada 2)
-ip link set enp1s0 down
+# 1. MATA o serviço de DHCP que está reconfigurando tudo
+systemctl stop dhcpcd
+systemctl disable dhcpcd
+killall dhcpcd 2>/dev/null
 
-# 2. Remove todos os endereços (Camada 3)
+# 2. Agora sim, limpa a interface e a rota
 ip addr flush dev enp1s0
-
-# 3. Mata a tabela de roteamento (O cérebro da rede)
-ip route flush cache
+ip link set enp1s0 down
 ip route del default 2>/dev/null
 
-# 4. Bloqueia tudo no Firewall (Caso o sistema tente reativar o IP)
-#iptables -P INPUT DROP
-#iptables -P FORWARD DROP
-#iptables -P OUTPUT DROP
+# 3. Limpa o DNS
+echo "" > /etc/resolv.conf
 
-# Limpa a tela e solta o desafio
+# 4. Avisa o aluno
 clear
 echo "################################################"
-echo "# SISTEMA ISOLADO - REDE DESATIVADA            #"
-echo "################################################"
-echo "# Desafio: Ative a interface enp1s0 e          #"
-echo "# configure o IP 172.30.1.10/24                #"
+echo "# ALERTA: SERVIÇO DHCP DESATIVADO              #"
+echo "# A rede foi removida manualmente.             #"
+echo "# Sua missão: Configurar IP e Rota via comando.#"
 echo "################################################"
