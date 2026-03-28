8ac904ae74070d4925e31b24006679f27aea8fdd
CARLOS REBOLLO <crebollo@gmail.com>
2026-03-28 11:32:28 -0300

    Create background.sh

---
+++ background.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Remove o gateway padrão (impede saída para internet)
+ip route del default
+
+# Bloqueia tráfego de saída via Firewall (opcional, mais agressivo)
+#iptables -A OUTPUT -o eth0 -j DROP
+
+# Mantém apenas a interface de loopback e a rede interna se necessário
